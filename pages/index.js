c82aab6e9a37e08c8f364df1bcff175d271cf0c7
Henrique Peixoto <henrique.peixoto.ribeiro50@gmail.com>
2021-01-25 17:10:49 -0300

    Result from the first day

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/widget'
+import Footer from '../src/components/footer'
+import GitHubCorner from '../src/components/gitHubCorner'
+import QuizBackground from '../src/components/quizBackground'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
+`
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            CSS
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>Lorem ipsum dolor...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl={'https://github.com/Henrique-Peixoto'} />
+    </QuizBackground>
+  )
 }
